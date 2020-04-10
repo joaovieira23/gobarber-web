b6a787f09277d9b434b1c1c2ea374ddcc5f34796
Joao Victor <joaovictorvieira.23@hotmail.com>
2020-04-10 16:31:10 -0300

    feat: private routes

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,5 +32,7 @@ module.exports = {
     'no-console': ['error', { allow: ['tron'] }],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+
+    'react/jsx-props-no-spreading': 0,
   },
 };
